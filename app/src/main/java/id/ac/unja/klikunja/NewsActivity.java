b28a166ee3dca1137fcf7fa984801c4b6925cb4c
Norman Syarif <normansyarif@icloud.com>
2019-02-28 22:51:17 +0700

    Add swipe to refresh feature

--- app/src/main/java/id/ac/unja/klikunja/NewsActivity.java
+++ app/src/main/java/id/ac/unja/klikunja/NewsActivity.java
@@ -2,6 +2,7 @@ package id.ac.unja.klikunja;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -11,6 +12,8 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.support.v7.widget.Toolbar;
 
@@ -25,20 +28,24 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class NewsActivity extends AppCompatActivity {
+public class NewsActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     public static final String API_KEY = "30bcbecb97ff41e4bdc4b4086b073d85";
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private List<Article> articles = new ArrayList<>();
     private Adapter adapter;
-    Toolbar newsToolbar;
+    private Toolbar newsToolbar;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private TextView whatsNew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
 
+        whatsNew = findViewById(R.id.text_whatsnew);
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
         newsToolbar = findViewById(R.id.news_toolbar);
         recyclerView = findViewById(R.id.recyclerView);
         layoutManager = new LinearLayoutManager(NewsActivity.this);
@@ -49,10 +56,18 @@ public class NewsActivity extends AppCompatActivity {
         setSupportActionBar(newsToolbar);
         getSupportActionBar().setTitle("News");
 
-        loadJson("");
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);
+
+        whatsNew.setVisibility(View.INVISIBLE);
+
+        onLoadingSwipeRefresh("");
     }
 
     public void loadJson(final String keyword) {
+
+        swipeRefreshLayout.setRefreshing(true);
+
         ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
 
         String country = Utils.getCountry();
@@ -75,19 +90,24 @@ public class NewsActivity extends AppCompatActivity {
                         articles.clear();
                     }
 
+                    whatsNew.setVisibility(View.VISIBLE);
                     articles = response.body().getArticle();
                     adapter = new Adapter(articles, NewsActivity.this);
                     recyclerView.setAdapter(adapter);
                     adapter.notifyDataSetChanged();
+                    swipeRefreshLayout.setRefreshing(false);
 
                 }else{
+                    whatsNew.setVisibility(View.INVISIBLE);
+                    swipeRefreshLayout.setRefreshing(false);
                     Toast.makeText(NewsActivity.this, "No result!", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(Call<News> call, Throwable t) {
-
+                whatsNew.setVisibility(View.INVISIBLE);
+                swipeRefreshLayout.setRefreshing(false);
             }
         });
 
@@ -106,7 +126,9 @@ public class NewsActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 if(query.length() > 2) {
-                    loadJson(query);
+                    onLoadingSwipeRefresh(query);
+                }else{
+                    Toast.makeText(NewsActivity.this, "Type more that 2 letters", Toast.LENGTH_SHORT).show();
                 }
                 return false;
             }
@@ -120,5 +142,19 @@ public class NewsActivity extends AppCompatActivity {
         searchMenuItem.getIcon().setVisible(false, false);
 
         return true;
+    }
+
+    @Override
+    public void onRefresh() {
+        loadJson("");
+    }
+
+    private void onLoadingSwipeRefresh(final String keyword) {
+        swipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                loadJson(keyword);
+            }
+        });
     }
 }
